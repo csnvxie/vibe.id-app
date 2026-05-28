1b1ed7eec17306b34bf53e4c1dc849cb69fd11f4
csnvxie <reksaqu@gmail.com>
2026-05-29 00:58:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -234,7 +234,6 @@ if menu == "Pembeli":
             st.session_state.beli_aktif = True
         
         if st.session_state.beli_aktif:
-        
         st.success(f"🎨 AI Berhasil Mendeteksi Warna Dominan: **{st.session_state.warna_terdeteksi}**")
         st.subheader("📦 Hasil Paket Rekomendasi VIBE-ID (Smart Bundle)")
         
