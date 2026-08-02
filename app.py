c16ae4fc06db1f973727bf90372794909b26319d
csnvxie <reksaqu@gmail.com>
2026-08-03 04:19:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ st.set_page_config(
     page_title="VIBE-ID — AI Outfit & Fashion Suite",
     page_icon="VIBEID LOGO.png",
     layout="wide",
-    initial_sidebar_state="expanded"
+    initial_sidebar_state="collapsed"
 )
 
 st.markdown("""
